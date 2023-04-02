c52ea491bab0ad878d80d236b01e59c2184cbaba
Pardhu <gpadhu1990@gmail.com>
2023-04-02 09:29:44 +0530

    react using map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
+export const App = () => {
+   const [data,setData] =useState({
+    username:'',
+    passwod:''
+   })
+   const {username,password} = data;
+   const changeHandler = e => {
+    setData({...data,[e.target.name]:[e.target.value]})
+   }
+   const submitHandler = e =>{
+    e.preventdefault()
+    console.log(data)
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+       <form onSubmit={submitHandler}>
+        <input type="text" name ="username" value={username} onChange={changeHandler}/><br/>
+        <input type="password" name ="password" value={password} onChange={changeHandler}/><br/>
+        <input type="submit" name="submit" />
+       </form> 
+      </center>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
